210fef9423ef733f0b780dc1a047d49e3501b7d1
Darya Shumitskiy <2198278s@student.gla.ac.uk>
2017-01-27 15:22:14 +0000

    Chapter 6 just before attempting end-of-chapter exercises

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+# Import Category model
+from rango.models import Category
+# Import Page model
+from rango.models import Page
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        # Get a category name slug with given name
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retrieve all of associated pages
+        pages = Page.objects.filter(category=category)
+
+        # Adds results list to template context under name pages
+        context_dict['pages'] = pages
+        # Also add category object from database to context dic
+        # Used in template to verify existence of category
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # Template will display 'no category' message
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
+    # Query database for list of all current categories
+    # Order by likes in non-ascending order
+    # Retrieve top 5 only
+    category_list = Category.objects.order_by('-likes')[:5]
+    # Put into dictionary to be passed to template
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
