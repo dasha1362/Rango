d7799b04c1eaf36aac1f140b34214c1981ff0d82
Darya Shumitskiy <2198278s@student.gla.ac.uk>
2017-01-27 13:50:53 +0000

    Chapter 5 fully complete

--- populate_rango.py
+++ populate_rango.py
@@ -34,13 +34,29 @@ def populate():
     ]
 
     # Create a dictionary of the categories you have created
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+    cats = {
+        "Python": {
+            "pages": python_pages,
+            "views": 128,
+            "likes": 64
+        },
+
+        "Django": {
+            "pages": django_pages,
+            "views": 64,
+            "likes": 32
+        },
+
+        "Other Frameworks": {
+            "pages": other_pages,
+            "views": 32,
+            "likes": 16
+        }
+    }
 
     # Calls add_cat() and add_page() repeatedly
     for cat, cat_data in cats.iteritems():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c,p["title"], p["url"])
 
@@ -58,8 +74,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
