85f0c9d1e32e326f84459f4aed46f6eecadbdaea
Darya Shumitskiy <2198278s@student.gla.ac.uk>
2017-02-01 13:46:36 +0000

    Chapter 7 complete

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,43 @@ from django.http import HttpResponse
 from rango.models import Category
 # Import Page model
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+
+def add_category(request):
+    form = CategoryForm()
+    # A HTTP post?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        # Valid form?
+        if form.is_valid():
+            # Save new category to database
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+            else:
+                print(form.errors)
+    context_dict = {'form': form, 'category': category}
+    return render(request,'rango/add_page.html', context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
